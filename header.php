087caba5d0f779b22573e20c04273cbae4becec2
James Kontargyris <james@jameskontargyris.co.uk>
2016-03-07 00:36:50 +0000

    Mobile rendering improvements

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
             <?php else : ?>
                 <a href="/rsvp" class="button button-primary smooth-scroll no-bottom-margin">RSVP</a>
             <?php endif; ?>
-            <a href="/contact-us" class="button button-secondary is-hidden-medium is-icon">
+            <a href="/contact-us" class="button button-secondary is-hidden-medium is-icon no-bottom-margin">
                 <img class="svg" src="<?php echo get_template_directory_uri(); ?>/img/envelope.svg" alt="Contact Us">
             </a>
         </section>
